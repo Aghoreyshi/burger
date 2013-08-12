6352b67b62a7a2aabff88e684f9315f1c4612d18
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-08-11 21:53:41 -0500

    Create the site skeleton

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+Burger
+====================
+
 A small-chain fast food burger joint's website
+---------------------
+
+I'm using Python Flask for this project.
--- burger.py
+++ burger.py
@@ -1,10 +1,40 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+#from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
 
+#----------------------------------------
+# controllers
+#----------------------------------------
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
-def burger():
-    return 'Burger Box'
+def index():
+    return render_template('index.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/locations')
+def locations():
+    return render_template('locations.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+#app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+    app.run()
